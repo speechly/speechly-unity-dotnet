d63ffd8b7d6f3a3e80bf147598a98c9a3ffcc466
Ari Nykänen <ari.o.nykanen@gmail.com>
2022-03-04 13:35:19 +0200

    Added speechly-client.unitypackage (snapshot of current progress)

--- unity-mic-to-speechly/Assets/SpeechlyTest.cs
+++ unity-mic-to-speechly/Assets/SpeechlyTest.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 
 public class SpeechlyTest : MonoBehaviour
 {
-    // Start is called before the first frame update
     async void Start()
     {
         Logger.Log = Debug.Log;
         await SpeechlyClientTest.test();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
